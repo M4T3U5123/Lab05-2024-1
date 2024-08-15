861bf3a6d20e162ae3fd15b54315db83c89f7b55
M4T3U5123 <119712880+M4T3U5123@users.noreply.github.com>
2024-08-15 18:26:37 +0000

    teste01

--- src/main/sorter/QuickSorter.java
+++ src/main/sorter/QuickSorter.java
@@ -7,7 +7,21 @@ public class QuickSorter implements Sorter {
     }
 
     public int partition(int[] values, int low, int high) {
-        return 0;
+        int piv = values[low];
+        int i = low;
+        int tempo = 0;
+        for(int j = low + 1; j <= high; j++){
+            if (values[j] <= piv){
+                i+=1;
+                tempo = values[i];
+                values[i]= values[j];
+                values[j] = tempo;
+            }
+        }
+        tempo = values[i];
+        values[i]= values[low];
+        values[low]= tempo;
+        return i;
     }
     
 }
